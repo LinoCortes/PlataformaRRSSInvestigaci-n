6a66979e6fee78370c68c8b064edd36905dcc429
Lino Cortes <lino.cortess@gmail.com>
2023-09-26 11:08:44 -0300

    first commit

---
+++ src/test/java/RRSSRD/com/example/demo/RrssrdApplicationTests.java
@@ -0,0 +1,13 @@
+package RRSSRD.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrssrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
